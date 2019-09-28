47a34646d107a7abc39b1bb82f3af17f6502f801
Celine Liew <celineliew.hx@gmail.com>
2019-09-28 00:00:47 -0700

    Create DOMtreeNode.js

---
+++ vanilla js/DOMtreeNode.js
@@ -0,0 +1,49 @@
+// Given two identical DOM tree structures, A and B, and a node from A, find the corresponding node in B.  
+
+const findNode = (rootA, rootB, targetNode) => {
+  if (rootA === targetNode) {
+    return rootB;
+  }
+  
+  let nodeB;
+  
+  while(!nodeB){
+    for (let i =0; i < rootA.childNodes.length; i++){
+      nodeB = findNode(rootA.childNodes[i], rootB.childNodes[i], targetNode);
+    }
+  }
+  return nodeB
+}
+
+// traverse the tree down in parallel.
+
+// method 2: travel up the node and find the path (no need to traverse whole tree)
+const getPathIndex = (node) => {
+  return Array.from(node.parentNode.childNodes).indexOf(node)
+}
+
+
+const getPath = (root, target) => {
+  let current = target;
+  let path = [];
+  
+  while (current !== root) {
+    let pathIndex = getPathIndex(current);
+    path.unshift(pathIndex);
+    current = current.parentNode;
+  }
+  return path;
+}
+
+const locateNode = (root, path) => {
+  let curr = root;
+  for (let idx of path) {
+    current = current.childNodes[idx];
+  }
+  return current;
+}
+
+const getNodeB = (rootA, rootB, target) => {
+  let path = getPath(rootA, target);
+  return locateNode(rootB, path);
+}
