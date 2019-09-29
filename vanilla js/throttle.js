9fd6e2d879cc77782c5d799809bcf0c003dc60ed
Celine Liew <celineliew.hx@gmail.com>
2019-09-28 17:47:36 -0700

    Update throttle.js

--- vanilla js/throttle.js
+++ vanilla js/throttle.js
@@ -1,18 +1,36 @@
+/* throttled - checks if timer not expired?  by the rate (number of events per unit time)*/
 
-/* throttled - checks if timer not expired?*/
 
-const throttle = (cb, limit) {
+/* using boolean */
+
+const throttle = (cb, limit) => {
   let wait = false;
   return (...args) => {
     if (!wait){
-      cb.apply(this,args);
+      cb.call(this,...args); //cb();
       wait = true;
       setTimeout( () => {
-        timer = false;
+        wait = false;
       }, limit);
     }
   };
 }
+
+function myFunc() {
+  console.log('Called');
+}
+
+
+let throttleFunc = throttle(myFunc, 3000);
+
+function handleClick() {
+  throttleFunc();
+}
+
+
+
+
+/* using timeout = null */
 
 
 function throttle(func, delay) {
@@ -31,3 +49,16 @@ function throttle(func, delay) {
 
 // throttle(function(arg1, arg2){console.log(arg1, arg2);}, 100)('red', 'blue');
 // red blue
+
+/* throttle by the delay between two handled events.  */
+
+function throttled(fn, delay){
+  let lastCall = 0;
+  return function(...args) {
+    const now = Date.now();
+    if (now - lastCall < delay){
+      return;
+    }
+    lastCall = now;
+    return fn(...args);
+}
