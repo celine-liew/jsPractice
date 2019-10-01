e5bd2014f2cba006379c0bee00bbe1df1622b640
Celine Liew <celineliew.hx@gmail.com>
2019-09-30 20:28:05 -0700

    Update partialApply.js

--- vanilla js/partialApply.js
+++ vanilla js/partialApply.js
@@ -20,4 +20,30 @@ console.log(twitterFavicon) // https://twitter.com/favicon.ico
 console.log(googleHome) // https://google.com/
 
 
+const partialApply = (fn, ...input1) => {
+  return (...input2) => {
+    return fn.apply( this, input1.concat(input2));
+  }
+}
+
+/* or this way */
+
+const partialApply2 = (fn, input1) => {
+  return (input2) => {
+    return fn.apply( this, [input1, input2]);
+  }
+}
+
+
+const add = (a,b) => a+b;
+let add5 = partialApply(add, 5);
+
+console.log(add5(10)) // 15
+
+
+let add10 = partialApply2(add, 10);
+
+console.log(add10(10)) //20
+
+
 
