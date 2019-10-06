3018b4766c2e5d257ba4d525b48a8f96a4efda2f
Celine Liew <celineliew.hx@gmail.com>
2019-10-05 23:00:16 -0700

    Update debounce.js

--- vanilla js/debounce.js
+++ vanilla js/debounce.js
@@ -71,3 +71,18 @@ hello there!!!!
 second second!!!!333
 second second!!!!22
 */
+
+    function debounce(func, wait = 20, immediate = true) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
